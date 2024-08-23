52a724a6e4c81847913981e76e4aca15dd54cf9c
Dear2ain <gentlejena1@gmail.com>
2024-08-23 18:27:15 +0900

    Fix Firebase initialization issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
