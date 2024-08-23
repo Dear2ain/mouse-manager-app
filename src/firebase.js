12b2f5241c9c36986b30452650802b662d672323
Dear2ain <gentlejena1@gmail.com>
2024-08-23 18:20:51 +0900

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // 여기에 Firebase 구성 객체를 붙여넣습니다
+  apiKey: "AIzaSyByjPntkkIviQvCHPzDjoFs8U_9RV5eRZU",
+  authDomain: "mouse-manager-app.firebaseapp.com",
+  projectId: "mouse-manager-app",
+  storageBucket: "mouse-manager-app.appspot.com",
+  messagingSenderId: "819515747633",
+  appId: "1:819515747633:web:b43f3bc63e2a2a58fe5410",
+  measurementId: "G-CEY4KC587H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
+
+export { db };
